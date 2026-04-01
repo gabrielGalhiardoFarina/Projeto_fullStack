89d22dca2e8ba272f87515ab2f0ff5197824af26
gabrielGalhiardoFarina <gabriel.farina@sptech.school>
2026-04-01 15:16:37 -0300

    feat: add screen registration

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,16 @@
-import { useEffect, useState } from 'react';
-import axios from 'axios';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Registration from "./screens/Registration";
+import './App.css';
 
 function App() {
-  const [dados, setDados] = useState(null);
-  const [erro, setErro] = useState('');
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Registration />
+    }
+  ]);
 
-  useEffect(() => {
-    axios
-      .get('http://localhost:5000/api/dados')
-      .then((response) => setDados(response.data))
-      .catch((e) => setErro(e.response?.data?.message || e.message));
-  }, []);
-
-  return (
-    <div>
-      <div className="card">
-        <h1>Bem-vindo ao meu projeto React!</h1>
-        <p>Este é um exemplo de aplicação React com Vite consumindo o backend.</p>
-        {erro && <p>Erro: {erro}</p>}
-        {!erro && !dados && <p>Carregando dados...</p>}
-        {dados && <p>{dados.mensagem}</p>}
-    
-      </div>
-    </div>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
